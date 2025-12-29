b8161034b49ceb3de6a39518b7d0354ed56097e6
Kitticai <michaelkitticai@gmail.com>
2025-12-29 03:58:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-serviceWorkerRegistration.register({
-  onUpdate: (registration) => {
-    window.dispatchEvent(new CustomEvent("timebox_sw_update", { detail: registration }));
-  },
-});
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
