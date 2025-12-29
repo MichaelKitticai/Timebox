a0f5a64926826dc33dc71b0354914ada32b7c375
Kitticai <michaelkitticai@gmail.com>
2025-12-29 03:18:01 +0300

    Update serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,30 +1,3 @@
-/* PREVIEW */
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-    window.location.hostname === "[::1]" ||
-    /^127(?:\.(?:25[0-5]|2[0-4]\d|[01]?\d?\d)){3}$/.test(window.location.hostname)
-);
-
-export function register(config) {
-  if (process.env.NODE_ENV !== "production") return;
-
-  if (!("serviceWorker" in navigator)) return;
-
-  const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-  if (publicUrl.origin !== window.location.origin) return;
-
-  window.addEventListener("load", () => {
-    const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-    if (isLocalhost) {
-      checkValidServiceWorker(swUrl, config);
-      navigator.serviceWorker.ready.then(() => {});
-    } else {
-      registerValidSW(swUrl, config);
-    }
-  });
-}
-
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
@@ -34,41 +7,17 @@ function registerValidSW(swUrl, config) {
         if (!installingWorker) return;
 
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === "installed") {
-            if (navigator.serviceWorker.controller) {
-              if (config && config.onUpdate) config.onUpdate(registration);
-            } else {
-              if (config && config.onSuccess) config.onSuccess(registration);
-            }
+          if (installingWorker.state !== "installed") return;
+
+          // New content is available
+          if (navigator.serviceWorker.controller) {
+            if (config && config.onUpdate) config.onUpdate(registration);
+          } else {
+            // First install
+            if (config && config.onSuccess) config.onSuccess(registration);
           }
         };
       };
     })
     .catch(() => {});
-}
-
-function checkValidServiceWorker(swUrl, config) {
-  fetch(swUrl, { headers: { "Service-Worker": "script" } })
-    .then((response) => {
-      const contentType = response.headers.get("content-type");
-      if (
-        response.status === 404 ||
-        (contentType && contentType.indexOf("javascript") === -1)
-      ) {
-        navigator.serviceWorker.ready
-          .then((registration) => registration.unregister())
-          .then(() => window.location.reload());
-      } else {
-        registerValidSW(swUrl, config);
-      }
-    })
-    .catch(() => {});
-}
-
-export function unregister() {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready
-      .then((registration) => registration.unregister())
-      .catch(() => {});
-  }
 }
